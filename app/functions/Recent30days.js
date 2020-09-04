18c8fde72ae095ab0dfc053ac0bed4585933fc66
Goldflag <bill2000yang@gmail.com>
2020-09-04 01:57:45 -0400

    added sessions stats

--- app/functions/Recent30days.js
+++ app/functions/Recent30days.js
@@ -1,19 +1,19 @@
 function recent30days(numEntries, currentTime, timeArr) {
     let snapShotExists = false;
     for (let i = numEntries - 1; i >= 0; --i) {
-        //if (currentTime - timeArr[i] < 47520) {
-        if (currentTime - timeArr[i] < 11520) {
+        if (currentTime - timeArr[i] < 47520) {
+        // if (currentTime - timeArr[i] < 11520) {
             snapShotExists = true;
         }
     }
     if (!snapShotExists) return 'none';
     //30 days is is 43200 minutes long
-    let margin = 10080; 
+    let margin = 43200; 
     let properIndex = 0;
     for (let i = numEntries - 1; i >= 0; --i) {
         let diff = currentTime - timeArr[i];
-        if (Math.abs(diff - 10080) < margin) {
-            margin = Math.abs(diff - 10080);
+        if (Math.abs(diff - 43200) < margin) {
+            margin = Math.abs(diff - 43200);
             properIndex = i;
         }
     }
