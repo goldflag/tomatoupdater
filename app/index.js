94ec1f4fb3215e4dd50c118cd06ee92353a439a4
Goldflag <bill2000yang@gmail.com>
2020-09-04 13:42:22 -0400

    new

--- app/index.js
+++ app/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(helmet());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://www.tomato.gg');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
