6ea9226d3bb87af7df480edf3074fd28c02861d5
Goldflag <bill2000yang@gmail.com>
2020-09-04 12:45:57 -0400

    c

--- app/index.js
+++ app/index.js
@@ -30,11 +30,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(helmet());
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://tomato.gg');
-//     res.header('Access-Control-Allow-Headers', "*");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://tomato.gg');
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+});
 
 /*
 There are 4 tables for the servers NA, EU, RU, and ASIA named devcom, deveu, devru, and devasia
