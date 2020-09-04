ea460b319271035ed63656571f94da43b6ada7ce
Goldflag <bill2000yang@gmail.com>
2020-09-04 14:20:31 -0400

    speedd

--- app/functions/recent3days.js
+++ app/functions/recent3days.js
@@ -1,22 +1,23 @@
-function recent3days(numEntries, currentTime, timeArr) {
+function recent30days(numEntries, currentTime, timeArr) {
     let snapShotExists = false;
     for (let i = numEntries - 1; i >= 0; --i) {
-        if (currentTime - timeArr[i] < 4600) {
+        //if (currentTime - timeArr[i] < 47520) {
+        if (currentTime - timeArr[i] < 11520) {
             snapShotExists = true;
         }
     }
     if (!snapShotExists) return 'none';
-    //a week is 10080 minutes long
-    let margin = 4320; 
+    //30 days is is 43200 minutes long
+    let margin = 10080; 
     let properIndex = 0;
     for (let i = numEntries - 1; i >= 0; --i) {
         let diff = currentTime - timeArr[i];
-        if (Math.abs(diff - 4320) < margin) {
-            margin = Math.abs(diff - 4320);
+        if (Math.abs(diff - 10080) < margin) {
+            margin = Math.abs(diff - 10080);
             properIndex = i;
         }
     }
     return properIndex;
 }
 
-module.exports = recent3days;
+module.exports = recent30days;
--- app/index.js
+++ app/index.js
@@ -13,9 +13,9 @@ const app = express();
 const tankStatsCompression = require('./functions/tankStatsCompression.js');
 //gives index of which stats json snapshot to attach to return file
 const recent24hr = require('./functions/Recent24hr.js');
-const recent3days = require('./functions/Recent3days.js');
-const recent1week = require('./functions/Recent3days.js');
-const recent30days = require('./functions/Recent1week.js');
+const recent1week = require('./functions/Recent1week.js');
+const recent30days = require('./functions/Recent30days.js');
+const recent60days = require('./functions/Recent60days.js');
 const recent1000 = require('./functions/Recent1000.js');
 const recent500 = require('./functions/Recent500.js');
 const calculateWN8 = require('./functions/calculateWN8.js')
@@ -114,9 +114,9 @@ app.get("/api/abcd/:server/:id", async (req, res) => {
                     overall: 'frog',
                     linegraph: [],
                     recent24hr: 'frog',
-                    recent3days: 'frog',
                     recent1week: 'frog',
                     recent30days: 'frog',
+                    recent60days: 'frog', 
                     recent1000: 'frog',
                     recent500: 'frog',
                 });             
@@ -128,9 +128,9 @@ app.get("/api/abcd/:server/:id", async (req, res) => {
                 const battlesArr = exists.rows[0].battlestamps;
                 // returns the index of respective stats snapshots for each period
                 const index24hr = recent24hr(numEntries, currentTime, timeArr);
-                const index3days = recent3days(numEntries, currentTime, timeArr);
                 const index1week = recent1week(numEntries, currentTime, timeArr);
                 const index30days = recent30days(numEntries, currentTime, timeArr);
+                const index60days = recent60days(numEntries, currentTime, timeArr);
                 const index1000 = recent1000(numEntries, compressedStats.battles, battlesArr);
                 const index500 = recent500(numEntries, compressedStats.battles, battlesArr);
 
@@ -188,9 +188,9 @@ app.get("/api/abcd/:server/:id", async (req, res) => {
                     linegraph: exists.rows[0].linegraph,
                     overall: compressedStats,
                     recent24hr: exists.rows[0].stats[index24hr] || 'frog',
-                    recent3days: exists.rows[0].stats[index3days] || 'frog',
                     recent1week: exists.rows[0].stats[index1week] || 'frog',
                     recent30days: exists.rows[0].stats[index30days] || 'frog',
+                    recent60days: exists.rows[0].stats[index60days] || 'frog',
                     recent1000: exists.rows[0].stats[index1000] || 'frog',
                     recent500: exists.rows[0].stats[index500] || 'frog',
                 }); 
@@ -200,7 +200,6 @@ app.get("/api/abcd/:server/:id", async (req, res) => {
         console.log(err);
     }
 });
-
 
 //default port is 5000
 const port = process.env.PORT || 5000;
