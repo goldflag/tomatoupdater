22e8778cf59330459109d248856e2a9cf3bbe16d
Goldflag <bill2000yang@gmail.com>
2020-09-04 13:04:38 -0400

    test

--- app/index.js
+++ app/index.js
@@ -66,168 +66,180 @@ SCHEMA OF TABLE FOR EACH SERVER:
 
 app.get("/api/abcd/:server/:id", async (req, res) => {
 
-    try {
-        let currentTime = parseInt(Date.now()/60000);
-        console.log(currentTime)
-        const server = req.params.server;
-        const id = req.params.id;      
+    // try {
+    //     let currentTime = parseInt(Date.now()/60000);
+    //     console.log(currentTime)
+    //     const server = req.params.server;
+    //     const id = req.params.id;      
 
-        let battles = 0;
-        let stats = {};
-        console.time("Time this");
+    //     let battles = 0;
+    //     let stats = {};
+    //     console.time("Time this");
 
-        let data1;
-        let data2;
-        await Promise.all([
-            fetch(`https://api.worldoftanks.${server}/wot/account/info/?application_id=${APIKey}&account_id=${id}`),
-            fetch(`https://api.worldoftanks.${server}/wot/tanks/stats/?application_id=${APIKey}&account_id=${id}&fields=mark_of_mastery%2C+tank_id%2C+all`),
-        ])
-        .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
-        .then(([d1, d2]) => 
-        {
-            data1 = d1;
-            data2 = d2;
-        });
+    //     let data1;
+    //     let data2;
+    //     await Promise.all([
+    //         fetch(`https://api.worldoftanks.${server}/wot/account/info/?application_id=${APIKey}&account_id=${id}`),
+    //         fetch(`https://api.worldoftanks.${server}/wot/tanks/stats/?application_id=${APIKey}&account_id=${id}&fields=mark_of_mastery%2C+tank_id%2C+all`),
+    //     ])
+    //     .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+    //     .then(([d1, d2]) => 
+    //     {
+    //         data1 = d1;
+    //         data2 = d2;
+    //     });
 
-        //number of battles overall an account has
-        battles = data1.data[id].statistics.all.battles;
-        if (battles > 0) {
-            //array of overall tank stats
-            stats = data2.data[id];
+    //     //number of battles overall an account has
+    //     battles = data1.data[id].statistics.all.battles;
+    //     if (battles > 0) {
+    //         //array of overall tank stats
+    //         stats = data2.data[id];
 
-            var t0 = performance.now()
-            // compresses stats file to save storage space. Also calculates accurate total battles
-            const compressedStats = tankStatsCompression(stats, currentTime, battles);
-            var t1 = performance.now()
-            //console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
+    //         var t0 = performance.now()
+    //         // compresses stats file to save storage space. Also calculates accurate total battles
+    //         const compressedStats = tankStatsCompression(stats, currentTime, battles);
+    //         var t1 = performance.now()
+    //         //console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
 
-            const exists = await db.query(`SELECT * FROM dev${server} WHERE player_id = $1`, [id]);
-            console.log('ID: ' + id);
-            // if player isn't already in the database, add to database and return empty stats
-            if (!exists.rows[0]) {
-                const WN8 = calculateWN8(stats);
-                console.log(`wn8: ${WN8}`);
-                const winrate = (data1.data[id].statistics.all.wins/battles);
-                console.log(`winrate: ${winrate}`);
-                const avgDamage = (data1.data[id].statistics.all.damage_dealt/battles);
-                console.log(`avgDamage: ${avgDamage}`);
-                const username = data1.data[id].nickname;
-                // new array of timestamps
-                const timeArr = [currentTime];
-                // new array of battlestamps
-                const battlesArr = [compressedStats.battles];
-                // new array of tank stats snapshots
-                const newEntry = [compressedStats];
-                // new array for line graph progression charts WN8/Winrate/DPG
-                const newLine = JSON.stringify([
-                    battles,
-                    WN8,
-                    winrate,
-                    avgDamage
-                ]);
+    //         const exists = await db.query(`SELECT * FROM dev${server} WHERE player_id = $1`, [id]);
+    //         console.log('ID: ' + id);
+    //         // if player isn't already in the database, add to database and return empty stats
+    //         if (!exists.rows[0]) {
+    //             const WN8 = calculateWN8(stats);
+    //             console.log(`wn8: ${WN8}`);
+    //             const winrate = (data1.data[id].statistics.all.wins/battles);
+    //             console.log(`winrate: ${winrate}`);
+    //             const avgDamage = (data1.data[id].statistics.all.damage_dealt/battles);
+    //             console.log(`avgDamage: ${avgDamage}`);
+    //             const username = data1.data[id].nickname;
+    //             // new array of timestamps
+    //             const timeArr = [currentTime];
+    //             // new array of battlestamps
+    //             const battlesArr = [compressedStats.battles];
+    //             // new array of tank stats snapshots
+    //             const newEntry = [compressedStats];
+    //             // new array for line graph progression charts WN8/Winrate/DPG
+    //             const newLine = JSON.stringify([
+    //                 battles,
+    //                 WN8,
+    //                 winrate,
+    //                 avgDamage
+    //             ]);
 
-                const newLineIns = [newLine];
-                const newPlayer = await db.query(`INSERT INTO dev${server} (player_id, username, numEntries, lastUpdate, timestamps, battlestamps, stats, linegraph) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) returning *`, 
-                    [id, username, 1, currentTime, timeArr, battlesArr, newEntry, newLineIns]);
-                res.status(200).json({ 
-                    status: 'success', 
-                    overall: 'frog',
-                    linegraph: [],
-                    recentsession: 'frog',
-                    recent24hr: 'frog',
-                    recent3days: 'frog',
-                    recent1week: 'frog',
-                    recent30days: 'frog',
-                    recent60days: 'frog', 
-                    recent1000: 'frog',
-                    recent500: 'frog',
-                });             
-            }
-            // if player already exists in database, return recent stats
-            else {
-                const numEntries = exists.rows[0].numentries;
-                const timeArr = exists.rows[0].timestamps;
-                const battlesArr = exists.rows[0].battlestamps;
-                // returns the index of respective stats snapshots for each period
-                const indexsession = recentsession(numEntries, compressedStats.battles, battlesArr);
-                const index24hr = recent24hr(numEntries, currentTime, timeArr);
-                const index3days = recent3days(numEntries, currentTime, timeArr);
-                const index1week = recent1week(numEntries, currentTime, timeArr);
-                const index30days = recent30days(numEntries, currentTime, timeArr);
-                const index60days = recent60days(numEntries, currentTime, timeArr);
-                const index1000 = recent1000(numEntries, compressedStats.battles, battlesArr);
-                const index500 = recent500(numEntries, compressedStats.battles, battlesArr);
+    //             const newLineIns = [newLine];
+    //             const newPlayer = await db.query(`INSERT INTO dev${server} (player_id, username, numEntries, lastUpdate, timestamps, battlestamps, stats, linegraph) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) returning *`, 
+    //                 [id, username, 1, currentTime, timeArr, battlesArr, newEntry, newLineIns]);
+    //             res.status(200).json({ 
+    //                 status: 'success', 
+    //                 overall: 'frog',
+    //                 linegraph: [],
+    //                 recentsession: 'frog',
+    //                 recent24hr: 'frog',
+    //                 recent3days: 'frog',
+    //                 recent1week: 'frog',
+    //                 recent30days: 'frog',
+    //                 recent60days: 'frog', 
+    //                 recent1000: 'frog',
+    //                 recent500: 'frog',
+    //             });             
+    //         }
+    //         // if player already exists in database, return recent stats
+    //         else {
+    //             const numEntries = exists.rows[0].numentries;
+    //             const timeArr = exists.rows[0].timestamps;
+    //             const battlesArr = exists.rows[0].battlestamps;
+    //             // returns the index of respective stats snapshots for each period
+    //             const indexsession = recentsession(numEntries, compressedStats.battles, battlesArr);
+    //             const index24hr = recent24hr(numEntries, currentTime, timeArr);
+    //             const index3days = recent3days(numEntries, currentTime, timeArr);
+    //             const index1week = recent1week(numEntries, currentTime, timeArr);
+    //             const index30days = recent30days(numEntries, currentTime, timeArr);
+    //             const index60days = recent60days(numEntries, currentTime, timeArr);
+    //             const index1000 = recent1000(numEntries, compressedStats.battles, battlesArr);
+    //             const index500 = recent500(numEntries, compressedStats.battles, battlesArr);
 
-                const newJson = {"wins": 12};
+    //             const newJson = {"wins": 12};
                 
-                console.log('no update');
-                console.log('battles dif: ' + (compressedStats.battles - battlesArr[battlesArr.length - 1]));
-                // Only updates stats if account has played at least one game since last snapshot
-                if ((compressedStats.battles - battlesArr[battlesArr.length - 1] > 0) || (currentTime - timeArr[timeArr.length - 1] > 360)) {
-                    console.log(`update happens battles passed:  ${compressedStats.battles - battlesArr[battlesArr.length - 1]} time:  ${currentTime - timeArr[timeArr.length - 1]}`);
-                    await db.query(
-                        `UPDATE dev${server} SET 
-                        numEntries = numEntries + 1, 
-                        lastUpdate = $2,
-                        timestamps = array_append(timestamps, $2),
-                        battlestamps = array_append(battlestamps, $3),
-                        stats = array_append(stats, $4)
-                        WHERE player_id = $1`,
-                        [id, currentTime, compressedStats.battles, compressedStats]);
-                }
-                if (compressedStats.battles - battlesArr[battlesArr.length - 1] > 10) {
-                    const WN8 = calculateWN8(stats);
-                    console.log(`wn8: ${WN8}`);
-                    const winrate = (data1.data[id].statistics.all.wins/battles);
-                    console.log(`winrate: ${winrate}`);
-                    const avgDamage = (data1.data[id].statistics.all.damage_dealt/battles);
-                    console.log(`avgDamage: ${avgDamage}`);
-                    const newLineIns = JSON.stringify([
-                        battles,
-                        WN8,
-                        winrate,
-                        avgDamage
-                    ]);
-                    await db.query(`UPDATE dev${server} SET linegraph = array_append(linegraph, $2) WHERE player_id = $1`, [id, newLineIns])
+    //             console.log('no update');
+    //             console.log('battles dif: ' + (compressedStats.battles - battlesArr[battlesArr.length - 1]));
+    //             // Only updates stats if account has played at least one game since last snapshot
+    //             if ((compressedStats.battles - battlesArr[battlesArr.length - 1] > 0) || (currentTime - timeArr[timeArr.length - 1] > 360)) {
+    //                 console.log(`update happens battles passed:  ${compressedStats.battles - battlesArr[battlesArr.length - 1]} time:  ${currentTime - timeArr[timeArr.length - 1]}`);
+    //                 await db.query(
+    //                     `UPDATE dev${server} SET 
+    //                     numEntries = numEntries + 1, 
+    //                     lastUpdate = $2,
+    //                     timestamps = array_append(timestamps, $2),
+    //                     battlestamps = array_append(battlestamps, $3),
+    //                     stats = array_append(stats, $4)
+    //                     WHERE player_id = $1`,
+    //                     [id, currentTime, compressedStats.battles, compressedStats]);
+    //             }
+    //             if (compressedStats.battles - battlesArr[battlesArr.length - 1] > 10) {
+    //                 const WN8 = calculateWN8(stats);
+    //                 console.log(`wn8: ${WN8}`);
+    //                 const winrate = (data1.data[id].statistics.all.wins/battles);
+    //                 console.log(`winrate: ${winrate}`);
+    //                 const avgDamage = (data1.data[id].statistics.all.damage_dealt/battles);
+    //                 console.log(`avgDamage: ${avgDamage}`);
+    //                 const newLineIns = JSON.stringify([
+    //                     battles,
+    //                     WN8,
+    //                     winrate,
+    //                     avgDamage
+    //                 ]);
+    //                 await db.query(`UPDATE dev${server} SET linegraph = array_append(linegraph, $2) WHERE player_id = $1`, [id, newLineIns])
 
-                }
-                // Removes oldest snapshot if it is more than 180 days old
-                if (currentTime - timeArr[0] > 259200) {
-                    console.log('delete old data');
-                    try {
-                        await db.query(`UPDATE dev${server} SET 
-                        timeStamps = timeStamps[2:],
-                        battleStamps = battleStamps[2:],
-                        stats = stats[2:],
-                        numEntries = numEntries - 1
-                        WHERE player_id = $1`, [id]);
-                    } catch(err) {
-                        console.log(err);
-                    }
-                }
-                res.status(200).json({ 
-                    server: server,
-                    username: exists.rows[0].username,
-                    status: 'success', 
-                    linegraph: exists.rows[0].linegraph,
-                    overall: compressedStats,                    
-                    recentsession: exists.rows[0].stats[indexsession] || 'frog',
-                    recent24hr: exists.rows[0].stats[index24hr] || 'frog',
-                    recent3days: exists.rows[0].stats[index3days] || 'frog',
-                    recent1week: exists.rows[0].stats[index1week] || 'frog',
-                    recent30days: exists.rows[0].stats[index30days] || 'frog',
-                    recent60days: exists.rows[0].stats[index60days] || 'frog',
-                    recent1000: exists.rows[0].stats[index1000] || 'frog',
-                    recent500: exists.rows[0].stats[index500] || 'frog',
-                }); 
-            }   
-        }
-    } catch(err) {
-        console.log(err);
-    }
-    //console.timeEnd("Time this");
-    console.timeEnd("Time this");
-
+    //             }
+    //             // Removes oldest snapshot if it is more than 180 days old
+    //             if (currentTime - timeArr[0] > 259200) {
+    //                 console.log('delete old data');
+    //                 try {
+    //                     await db.query(`UPDATE dev${server} SET 
+    //                     timeStamps = timeStamps[2:],
+    //                     battleStamps = battleStamps[2:],
+    //                     stats = stats[2:],
+    //                     numEntries = numEntries - 1
+    //                     WHERE player_id = $1`, [id]);
+    //                 } catch(err) {
+    //                     console.log(err);
+    //                 }
+    //             }
+    //             res.status(200).json({ 
+    //                 server: server,
+    //                 username: exists.rows[0].username,
+    //                 status: 'success', 
+    //                 linegraph: exists.rows[0].linegraph,
+    //                 overall: compressedStats,                    
+    //                 recentsession: exists.rows[0].stats[indexsession] || 'frog',
+    //                 recent24hr: exists.rows[0].stats[index24hr] || 'frog',
+    //                 recent3days: exists.rows[0].stats[index3days] || 'frog',
+    //                 recent1week: exists.rows[0].stats[index1week] || 'frog',
+    //                 recent30days: exists.rows[0].stats[index30days] || 'frog',
+    //                 recent60days: exists.rows[0].stats[index60days] || 'frog',
+    //                 recent1000: exists.rows[0].stats[index1000] || 'frog',
+    //                 recent500: exists.rows[0].stats[index500] || 'frog',
+    //             }); 
+    //         }   
+    //     }
+    // } catch(err) {
+    //     console.log(err);
+    // }
+    // //console.timeEnd("Time this");
+    // console.timeEnd("Time this");
+    res.status(200).json({ 
+        status: 'success', 
+        overall: 'frog',
+        linegraph: [],
+        recentsession: 'frog',
+        recent24hr: 'frog',
+        recent3days: 'frog',
+        recent1week: 'frog',
+        recent30days: 'frog',
+        recent60days: 'frog', 
+        recent1000: 'frog',
+        recent500: 'frog',
+    }); 
 });
 
 
