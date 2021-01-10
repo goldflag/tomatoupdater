7794172d9391f942627f0c0dd86b8164f62fdc1c
Goldflag <bill2000yang@gmail.com>
2021-01-09 22:05:20 -0500

    test

--- app/index.js
+++ app/index.js
@@ -9,6 +9,8 @@ const fetch = require('node-fetch');
 const tankNames = require('./data/tankNames.json');
 const WN8 = require('./data/wn8.json');
 const app = express();
+const fs = require('fs');
+
 
 //compresses tank stats into more readable form
 const IDupdater = require('./updater/IDupdater.js');
@@ -33,6 +35,48 @@ app.listen(port, () => {
 app.get("/updateFiles", async (req, res) => {
     updateFiles();
     res.status(200).send(`updating`);
+});
+
+app.get("/downloadFiles/:server", async (req, res) => {
+    let IDs;
+    if (req.params.server === "com") IDs = await db.query(`SELECT player_id from idscom where active = true`); 
+    else if (req.params.server === "eu") IDs = await db.query(`SELECT player_id from idseu where active = true`); 
+    else if (req.params.server === "asia") IDs = await db.query(`SELECT player_id from idsasia where active = true`); 
+
+
+    const arr = [];
+
+    for (let i = 0; i < IDs.rowCount; ++i) {
+        if (IDs.rows[i].player_id % 1000 === 0)
+            console.log(IDs.rows[i].player_id);
+            arr.push(IDs.rows[i].player_id);
+        if (i === IDs.rowCount-1) {
+            let data = JSON.stringify(arr, null, 2);
+            await db.query(`UPDATE IDs SET ids = $1, size = $2 WHERE server = $3`, [arr, arr.length, req.params.server]);
+
+            fs.writeFile(`./app/data/${req.params.server}IDs.json`, data, (err) => {
+                if (err) throw err;
+                console.log(`${req.params.server}IDs.json Written`);
+            });
+
+        }
+    }
+
+    
+    // const arr = [];
+    // for (let i = 0; i < IDs.length; ++i) {
+    //     arr.push(IDs.rows[i].player_id);
+    //     console.log(IDs.rows[i].player_id)
+    //     if (i === IDs.length - 1) {
+    //         let data = JSON.stringify(arr, null, 2);
+    //         fs.writeFile(`${req.params.server}IDs.json`, data, (err) => {
+    //             if (err) throw err;
+    //             console.log(`${req.params.server}IDs.json Written`);
+    //         });        
+    //     }
+    // }
+    // console.log(arr);
+    res.status(200).send(`downloaded`);
 });
 
 app.get("/update/:server", async (req, res) => {
--- app/updater/updater.js
+++ app/updater/updater.js
@@ -4,10 +4,13 @@ const async  = require('express-async-await');
 
 async function playerUpdater(server) {
     let IDs;
-    if (server === "com") IDs = await db.query(`SELECT player_id from idscom where active = true`); 
-    else if (server === "eu") IDs = await db.query(`SELECT player_id from idseu where active = true`); 
-    else if (server === "asia") IDs = await db.query(`SELECT player_id from idsasia where active = true`); 
+    // if (server === "com") IDs = await db.query(`SELECT player_id from idscom where active = true`); 
+    // else if (server === "eu") IDs = await db.query(`SELECT player_id from idseu where active = true`); 
+    // else if (server === "asia") IDs = await db.query(`SELECT player_id from idsasia where active = true`); 
 
+    if (server === "com") IDs = await db.query(`SELECT ids from ids where server = 'com'`); 
+    else if (server === "eu") IDs = await db.query(`SELECT ids from ids where server = 'eu'`); 
+    else if (server === "asia") IDs = await db.query(`SELECT ids from ids where server = 'asia'`); 
     console.log(IDs.rows.length);
 
     IDs = IDs.rows;
@@ -16,12 +19,13 @@ async function playerUpdater(server) {
     for (let i = 0; i < IDs.length; ++i) {
         setTimeout(function () {
             // console.log(`count: ${i} id: ${IDs[i + 0]}`);
-            APIcall(count, IDs[i + 0].player_id);
+            // APIcall(count, IDs[i + 0].player_id);
+            APIcall(count, IDs[i + 0]);
             count++;
             if (count == 22) {
                 count = 1;
             }
-        }, i * 30);
+        }, i * 20);
     }
 
     async function APIcall(count, id) {
