f331d8208bd92d22dff23db6057bf519582eec31
Goldflag <bill2000yang@gmail.com>
2021-01-12 20:39:19 -0500

    slower

--- app/updater/updater.js
+++ app/updater/updater.js
@@ -18,16 +18,16 @@ async function playerUpdater(server) {
     // console.log(IDs);
 
     let count = 1;
-    for (let i = 0; i < IDs.length; ++i) {
+    for (let i = 0; i < IDs.length - 242929; ++i) {
         setTimeout(function () {
             // console.log(`count: ${i} id: ${IDs[i + 0]}`);
             // APIcall(count, IDs[i + 0].player_id);
-            APIcall(count, IDs[i]);
+            APIcall(count, IDs[i + 242929]);
             count++;
             if (count == 22) {
                 count = 1;
             }
-        }, i * 20);
+        }, i * 27);
     }
 
     async function APIcall(count, id) {
